b5e3565e736bf54c6e78b2c8dfb63c0892ee4bbf
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-09-03 14:17:12 -0400

    Added app urls

---
+++ store/app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from app import views
+from django.view.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='app/index.html', name='index'))
+]
