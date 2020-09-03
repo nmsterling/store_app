15318aed82d6463b9195b102e26f88cf39ee1c86
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-09-03 14:59:36 -0400

    Fixed another typo

--- store/app/urls.py
+++ store/app/urls.py
@@ -3,5 +3,5 @@ from app import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='app/index.html', name='index'))
+    path('', TemplateView.as_view(template_name='app/index.html'), name='index')
 ]
