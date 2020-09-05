2c1cf5f758d634d1ad9ff4f549797c5b40ec340e
Natalie Sterling <nataliemetasterling@gmail.com>
2020-09-05 09:45:55 -0400

    added login system(regitration folder with login.html, routes update in url and settings, crispy forms installed in virtual enviroment), added base.html (kept main javascript in index.html for now)

--- store/store/urls.py
+++ store/store/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
+    path('', include('app.urls')),
     path('api/', include('api.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
