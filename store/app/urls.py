e6ec4a733934116bdb254deedf1d679890f30591
Natalie Sterling <nataliemetasterling@gmail.com>
2020-09-07 08:25:25 -0400

    set up cart path, can click on cart from logged in home page, cart currently connects to vue, added cart.html

--- store/app/urls.py
+++ store/app/urls.py
@@ -3,5 +3,6 @@ from app import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='app/index.html'), name='index')
+    path('', TemplateView.as_view(template_name='app/index.html'), name='index'),
+    path('cart/', TemplateView.as_view(template_name='app/cart.html'), name='cart')
 ]
