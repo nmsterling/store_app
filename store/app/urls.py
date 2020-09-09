ce1e9322a394054a5863e96d6398bd216591379c
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2020-09-08 20:44:34 -0400

    everything

--- store/app/urls.py
+++ store/app/urls.py
@@ -9,7 +9,9 @@ app_name = 'app'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='app/index.html'), name='index'),
+    path('categories/', TemplateView.as_view(template_name='app/categories.html'), name='categories'),
     path('shop/', views.list_products, name="list_products"),
+    path('electric/', views.list_electric, name='electric'),
     path('cart/', TemplateView.as_view(template_name='app/cart.html'), name='cart')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/app/views.py
+++ store/app/views.py
@@ -3,8 +3,18 @@ from django.shortcuts import render
 from api.models import Products
 
 # Create your views here.
+
 def list_products(request):
     products = Products.objects.all()
     return render(request, 'app/products.html', {
         "products": products
     })
+
+def list_electric(request):
+    products = Products.objects.filter(category="Electric Guitar")
+    return render(request, 'app/electric.html', {
+        "products": products
+    })
+
+# create the filtered views...
+# def list_acoustic(request):
