59659fff4e5feff30bf666dc97236d0b909556e3
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2020-09-08 22:33:34 -0400

    added shop by category, a bunch of new templates and updated urls, I intend to look up how to put this into one class based view

--- store/app/urls.py
+++ store/app/urls.py
@@ -5,13 +5,16 @@ from django.conf.urls.static import static
 from app import views
 from django.views.generic import TemplateView
 
-app_name = 'app'
+# app_name = 'app'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='app/index.html'), name='index'),
     path('categories/', TemplateView.as_view(template_name='app/categories.html'), name='categories'),
     path('shop/', views.list_products, name="list_products"),
     path('electric/', views.list_electric, name='electric'),
+    path('acoustic/', views.list_acoustic, name='acoustic'),
+    path('amps/', views.list_amps, name='amps'),
+    path('cases/', views.list_cases, name='cases'),
     path('cart/', TemplateView.as_view(template_name='app/cart.html'), name='cart')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/app/views.py
+++ store/app/views.py
@@ -4,6 +4,8 @@ from api.models import Products
 
 # Create your views here.
 
+# Look up how to put these all into a Class Based APIList View with defs or something
+
 def list_products(request):
     products = Products.objects.all()
     return render(request, 'app/products.html', {
@@ -12,9 +14,24 @@ def list_products(request):
 
 def list_electric(request):
     products = Products.objects.filter(category="Electric Guitar")
-    return render(request, 'app/electric.html', {
+    return render(request, 'app/category_filter.html', {
         "products": products
     })
 
-# create the filtered views...
-# def list_acoustic(request):
+def list_acoustic(request):
+    products = Products.objects.filter(category="Acoustic")
+    return render(request, 'app/category_filter.html', {
+        "products": products
+    })
+
+def list_amps(request):
+    products = Products.objects.filter(category="Amps")
+    return render(request, 'app/category_filter.html', {
+        "products": products
+    })
+
+def list_cases(request):
+    products = Products.objects.filter(category="Cases")
+    return render(request, 'app/category_filter.html', {
+        "products": products
+    })
