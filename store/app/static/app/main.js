aa7af0861d5ead9f615e7d5a8a70bb03e3152530
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-09-10 17:01:08 -0400

    Set up axios

--- store/app/static/app/main.js
+++ store/app/static/app/main.js
@@ -26,5 +26,33 @@ userProfile = new Vue({
     el: '#userProfile',
     data: {
         message: 'Account',
+        info: [],
+        profiles: [
+        {
+            user: 'Alice',
+            email: 'alice@store.com',
+            address: '123 W Trindle Rd, Mechanicsburg PA 17055',
+            preferred: false,
+        },
+        {
+            user: 'Bob',
+            email: 'bob@store.com',
+            address: '456 Simpson St, Mechanicsburg PA 17055',
+            preferred: false,
+        },
+        {
+            user: 'Charlie',
+            email: 'charlie@store.com',
+            address: '555 Marble St, Mechanicsburg PA 17055',
+            preferred: false,
+        },
+        ]
+
+        
+    },
+    mounted() {
+        axios
+            .get('/api/profile')
+            .then(response => (this.info = response.data))
     }
 })
