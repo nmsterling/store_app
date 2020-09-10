d71e2149c7af1d52fca34201693291314cd20a84
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-09-10 14:54:41 -0400

    Adding some tests

--- store/app/tests.py
+++ store/app/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from api.models import Profile, Products, Reviews, TransactionsHistory, Cart
 
 # Create your tests here.
+
+class UserProfileTestCase(TestCase):
+   
+    def test_account_renders_correct_status_code(self):
+        c = Client()
+        response = c.get('account/')
+        self.assertEqual(response.status_code, 404)
+
+    # def test_account_renders_correct_template(self):
+    #     c = Client()
+    #     response = c.get('account/')
+    #     self.assertTemplateUsed(response, 'app/account.html')
