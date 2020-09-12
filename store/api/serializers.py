2001af87c22db4fe34e41b956867f08b8fd2c36d
Natalie Sterling <nataliemetasterling@gmail.com>
2020-09-12 15:25:13 -0400

    added item_total function and field to cartserializer

--- store/api/serializers.py
+++ store/api/serializers.py
@@ -27,9 +27,23 @@ class TransactionHistorySerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'product_name', 'quantity_purchased', 'transaction_total', 'timestamp']
 
 class CartSerializer(serializers.ModelSerializer):
+    item_total = serializers.SerializerMethodField('get_item_total')
+    # sub_total = serializers.SerializerMethodField('get_sub_total')
+
+    def get_item_total(self, cart):
+        return cart.product_name.price * cart.quantity
+
+    #     def get_sub_total(self, cart):
+    #         sub_total = 0
+    #         for user in cart.user:
+
+    #             for product in cart.product_name:
+    #                 item_total = product.price * cart.quantity
+    #                 sub_total += item_total
+    #         return sub_total
 
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'product_name', 'quantity']
+        fields = ['id', 'user', 'product_name', 'quantity', 'item_total']
         # this specifies the depth of the relationships for related tables and populates those in json
         depth = 2
