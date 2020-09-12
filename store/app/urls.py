81e5798445e7764fef8b82a8d2e94df0d2276139
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2020-09-12 13:33:14 -0400

    replaced index.html with function based view such that I could pass Cart query to it conditionally, added cart button to base.html conditionally as well

--- store/app/urls.py
+++ store/app/urls.py
@@ -8,11 +8,13 @@ from django.views.generic import TemplateView
 app_name = 'app'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='app/index.html'), name='index'),
+    # path('', TemplateView.as_view(template_name='app/index.html'), name='index'),
+    path('', views.index, name='index'),
     path('categories/', views.categories, name='categories'),
     path('shop/', views.list_products, name="list_products"),
     path('cart/', TemplateView.as_view(template_name='app/cart.html'), name='cart'),
     path('search/', views.categories_list_view, name="categories_list_view"),
+    path('cart/<str:product>', views.add_to_cart, name='cart_add'),
     path('filter/<str:category>/', views.filter_products, name="filter"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
