3e6ba237b7d9b6c63d853eb7af795e17d490da0e
Natalie Sterling <nataliemetasterling@gmail.com>
2020-09-16 10:20:20 -0400

    tests to make sure 200 code for the following pages: cart, checkout, shop, index, account

--- store/api/tests.py
+++ store/api/tests.py
@@ -1,3 +1,33 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APIClient, APIRequestFactory
+from .models import Cart, Profile, Products, TransactionsHistory
+
+class TestPages(TestCase):
+    def test_cart(self):
+        self.client = APIClient()
+        response = self.client.get("/cart/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_checkout(self):
+        self.client = APIClient()
+        response = self.client.get("/checkout/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_shop(self):
+        self.client = APIClient()
+        response = self.client.get("/shop/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index(self):
+        self.client = APIClient()
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_account(self):
+        self.client = APIClient()
+        response = self.client.get("/account/")
+        self.assertEqual(response.status_code, 200)
+
